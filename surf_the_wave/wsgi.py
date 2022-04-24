78362b92fa24b8dfceb61a5701861d1b74394f12
anyahush <anyahush@gmail.com>
2022-04-24 09:23:53 +0000

    Install Django and create project app

---
+++ surf_the_wave/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for surf_the_wave project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surf_the_wave.settings')
+
+application = get_wsgi_application()
