8f8bbfc4deb023675fd635978419f0ad7e7df7d6
anyahush <anyahush@gmail.com>
2022-04-28 10:57:05 +0000

    Add sorting functionality to all_products view

--- products/models.py
+++ products/models.py
@@ -5,13 +5,13 @@ class Category(models.Model):
     """ Creates category table in database """
     class Meta:
         verbose_name_plural = 'Categories'
-    
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
