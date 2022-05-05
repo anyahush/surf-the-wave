c04b416fa56cac2134243e20cf0c94391e109d89
anyahush <anyahush@gmail.com>
2022-05-05 17:28:02 +0000

    Add basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,4 @@
+// Declare variables
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -26,3 +27,48 @@ var style = {
 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Create change event and displays error message
+// if error occurs
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if(event.error) {
+        var html = `
+        <span rold="aler">
+            <i class="fas fa-times></i>
+        </span>
+        <span>${event.error.message}</span>`
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disbaled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if(result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span rold="aler">
+                <i class="fas fa-times></i>
+                </span>
+                <span>${event.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disbaled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentInten.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    })
+})
