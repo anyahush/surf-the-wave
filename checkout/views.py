347b09cd5375e6f412af70057ebd4a4aab87a9bf
anyahush <anyahush@gmail.com>
2022-05-08 21:20:12 +0000

    Allow users to save personal info to profile and view order history

--- checkout/views.py
+++ checkout/views.py
@@ -8,8 +8,11 @@ import json
 
 from basket.contexts import basket_contents
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 
 
 @require_POST
@@ -83,7 +86,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_basket'))
 
-            request.session['save-info'] = 'save-info' in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
 
         else:
@@ -122,13 +125,37 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_full_name': order.full_name,
+                'default_email': order.email,
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order succesfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
-    
+
     if 'basket' in request.session:
         del request.session['basket']
-    
+
     template = 'checkout/checkout_success.html'
 
     context = {
