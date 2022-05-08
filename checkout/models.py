99a0e6e49cdc6b5a2e4a60c0be3e071cce2867eb
anyahush <anyahush@gmail.com>
2022-05-08 11:36:50 +0000

    Finish webhook handler to get or create order info to store in db

--- checkout/models.py
+++ checkout/models.py
@@ -18,12 +18,14 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=False, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=False, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=False, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_basket = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
