4af06a5dc20237965c4ed5cc8645523f7396695d
anyahush <anyahush@gmail.com>
2022-05-11 14:52:12 +0000

    Create blog model class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    """ Creates Blog table in database """
+    blog_title = models.CharField(max_length=254)
+    blog_content = models.TextField()
+    author = models.CharField(max_length=254)
+    image = models.ImageField()
+    date_created = models.DateTimeField()
+
