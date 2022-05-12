c380949e4888d10e88477f8bc691afa550dd7268
anyahush <anyahush@gmail.com>
2022-05-12 19:07:25 +0000

    Add edit_blog view, urls and template

--- blog/views.py
+++ blog/views.py
@@ -59,3 +59,31 @@ def add_blog(request):
 
     return render(request, template, context)
 
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Admin users can edit blogs on the site """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, you do not have permission to do this')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Blog successfully updated')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to updated blog. Please ensure the form is valid')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {blog.blog_title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
