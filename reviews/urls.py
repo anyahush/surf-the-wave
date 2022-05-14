e52ace05fc0bf4f4b168a77c98b00c856792f182
anyahush <anyahush@gmail.com>
2022-05-14 17:53:18 +0000

    Create edit_product_review view, urls and template

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:product_id>/add/', views.create_review, name='create_review'),
+    path('<int:review_id>/edit/', views.edit_product_review, name='edit_product_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from products.models import Product
@@ -8,6 +9,7 @@ from reviews.models import ProductReview
 from .forms import ReviewForm
 
 
+@login_required
 def create_review(request, product_id):
     """ A view to allow users to create product reviews """
     product = get_object_or_404(Product, pk=product_id)
@@ -30,6 +32,35 @@ def create_review(request, product_id):
     context = {
         'product': product,
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_product_review(request, review_id):
+    """ A view to all users to edit product reviews"""
+
+    review = get_object_or_404(ProductReview, id=review_id)
+    product = review.product
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Review successfully updated')
+            return redirect(reverse('product_detail', kwargs={"product_id": product.id}))
+        else:
+            messages.error(request, 'Failed to updated review. Please ensure the form is valid')
+    else:
+        form = ReviewForm(instance=review)
+        messages.info(request, f'You are editing a review for {product.name}')
+
+    template = 'reviews/edit_review.html'
+    context = {
+        'form': form,
+        'product': product,
+        'review': review,
     }
 
     return render(request, template, context)
