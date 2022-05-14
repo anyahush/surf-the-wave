723d9a500677a4bacd560db421eab83aae12d64b
anyahush <anyahush@gmail.com>
2022-05-14 20:33:57 +0000

    Add to contact view to display contact form

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+
+from .forms import ContactForm
 
 
 def contact(request):
     """ A view to render the contact page """
 
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        # If post method successful get information from form
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email_from': request.POST['email_from'],
+            'order_number': request.POST['order_number'],
+            'enquiry': request.POST['enquiry'],
+        }
+        contact_form = ContactForm(form_data)
+
+        if contact_form.is_valid():
+            customer_contact = contact_form.save(commit=False)
+            messages.success(request, 'Your enquiry has been sent. Check your emails for a confirmation')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'There was an error with your enquiry. Please ensure the form is valid')
+            return redirect(reverse('contact'))
+    
+    contact_form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, template, context)
