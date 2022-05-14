226e2012e5331fcbb50d2f14a2feaa1f299165a2
anyahush <anyahush@gmail.com>
2022-05-14 15:31:06 +0000

    Amend create_review view to post reviews to db

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.models import User
 
 from products.models import Product
 from reviews.models import ProductReview
@@ -10,23 +11,20 @@ from .forms import ReviewForm
 def create_review(request, product_id):
     """ A view to allow users to create product reviews """
     product = get_object_or_404(Product, pk=product_id)
-    form = ReviewForm
 
     if request.method == 'POST':
-        previous_review = ProductReview.objects.filter(
-            author=request.user
-        ).exists()
-        if previous_review:
-            messages.error(request, f'You have already left a review for this product on {previous_review.date_added}')
-        else:
-            form = ReviewForm(request.POST)
-            if form.is_valid():
-                review = form.save(commit=False)
-                messages.success(request, 'Successfully added review!')
-            else:
-                messages.error(request, 'Failed to add review. Please ensure the form is valid')
-
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = User.objects.get(username=request.user.username)
+            review.product = product
+            review.save()
+            messages.success(request, 'Successfully added review!')
             return redirect(reverse('product_detail', kwargs={"product_id": product.id}))
+        else:
+            messages.error(request, 'Failed to add review. Please ensure the form is valid')
+    else:
+        form = ReviewForm()
 
     template = 'reviews/create_review.html'
     context = {
