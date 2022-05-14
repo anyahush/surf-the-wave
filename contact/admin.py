a9049a9dfc7686dca9ae1a7b6e10064083724b0e
anyahush <anyahush@gmail.com>
2022-05-14 20:42:29 +0000

    Add contact admin class and migrate admin model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """ Outlines what fields we want admin to see """
+    list_display = (
+        'full_name',
+        'email_from',
+        'order_number',
+        'enquiry',
+        'enquiry_sent',
+    )
+    ordering = ('-enquiry_sent',)
+
+
+admin.site.register(Contact, ContactAdmin)
