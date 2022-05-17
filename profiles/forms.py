b40b434e04dc5e123748f33581d6541689b36990
anyahush <anyahush@gmail.com>
2022-05-17 16:46:39 +0000

    Remove unnecessary class from profile form

--- profiles/forms.py
+++ profiles/forms.py
@@ -34,5 +34,4 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'profile-input'
             self.fields[field].label = False
