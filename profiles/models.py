95ae3e196bb3e62eefe098a8f19d5f07958a7f96
anyahush <anyahush@gmail.com>
2022-05-21 15:56:21 +0000

    Add regex validator to phone field in profile app

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -14,7 +15,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_full_name = models.CharField(max_length=20, null=True, blank=True)
     default_email = models.EmailField(max_length=20, null=True, blank=True)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, validators=[
+                                            RegexValidator(r'^\+?\d{11,15}$')],
+                                            error_messages={'invalid':
+                                            'invalid phone number'},
+                                            blank=True, null=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
