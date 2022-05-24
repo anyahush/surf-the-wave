4d3ba992a5b0c80ea296316ecdd87296f4c3093f
anyahush <anyahush@gmail.com>
2022-05-24 20:05:28 +0000

    Fix PEP8 errors

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,14 +1,13 @@
+import json
+import time
+
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-
-from .models import Order, OrderLineItem
-from products.models import Product
 from profiles.models import UserProfile
-
-import json
-import time
+from products.models import Product
+from .models import Order, OrderLineItem
 
 
 class StripeWH_Handler:
@@ -69,8 +68,10 @@ class StripeWH_Handler:
                 profile.default_full_name = shipping_details.name
                 profile.default_email = billing_details.email
                 profile.default_phone_number = shipping_details.phone
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
                 profile.default_town_or_city = shipping_details.address.city
                 profile.default_county = shipping_details.address.state
                 profile.default_postcode = shipping_details.address.postal_code
@@ -103,7 +104,8 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                    content=f'Webhook received: {event["type"]} | SUCCESS: Verified order in database',
+                    content=f'Webhook received: {event["type"]} | SUCCESS: \
+                              Verified order in database',
                     status=200)
         else:
             order = None
@@ -132,7 +134,8 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in \
+                         item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -148,7 +151,8 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created in the database',
+            content=f'Webhook received: {event["type"]} | SUCCESS: \
+                      Created in the database',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
